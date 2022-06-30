0594c6deb63da5ba2adbb23baa663bcdf6f8fc16
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-06-30 13:21:35 -0400

    Adds routes and controller methods for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def all_products
     render json: { product_data: Product.all }
   end
+
+  def headphones
+    render json: { product_data: Product.first }
+  end
+
+  def skillet
+    render json: { product_data: Product.find_by(id: 2) }
+  end
+
+  def sunglasses
+    render json: { product_data: Product.find_by(id: 3) }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+  get "/headphones", controller: "products", action: "headphones"
+  get "/skillet", controller: "products", action: "skillet"
+  get "/sunglasses", controller: "products", action: "sunglasses"
 end
