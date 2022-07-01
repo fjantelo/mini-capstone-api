917957c106f41f0eb1072d850c71a169532f1421
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-01 13:45:12 -0400

    Adds create method to add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,14 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: "JanSport JS0A4QUE008",
+      price: 39,
+      image_url: "https://m.media-amazon.com/images/I/817EuMPlOOL._AC_SL1500_.jpg",
+      description: "JanSport JS0A4QUE008 Superbreak Plus Black",
+    )
+    product.save
+  end
 end
