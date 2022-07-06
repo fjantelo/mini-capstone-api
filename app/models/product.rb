e98da36dd4f647d23ebc406b725c653fdc3a1e03
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-06 12:50:31 -0400

    Adds is_discounted?, tax, and total model methods to products controller and show action

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_dicounted?
+    if price < 10
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
--- terminal_products.rb
+++ terminal_products.rb
@@ -1,33 +1,39 @@
 require "http"
 system "clear"
 
-while true
-  puts "Welcome to the Mini Capstone API Store. Enter a page number:"
-  puts "0: All products"
-  puts "1: Headphones"
-  puts "2: Skillet"
-  puts "3: Sunglasses"
-  puts ""
-  puts "Type 'exit' to exit the store."
-  selection = gets.chomp
+# while true
+#   puts "Welcome to the Mini Capstone API Store. Enter a page number:"
+#   puts "0: All products"
+#   puts "1: Headphones"
+#   puts "2: Skillet"
+#   puts "3: Sunglasses"
+#   puts ""
+#   puts "Type 'exit' to exit the store."
+#   selection = gets.chomp
 
-  if selection == "0"
-    response = HTTP.get("http://localhost:3000/products")
-    index = 0
-    while index < response.parse(:json).length
-      puts "#{index + 1}. #{response.parse(:json)[index]["name"]}"
-      index += 1
-    end
-  elsif selection == "1" || selection == "2" || selection == "3"
-    response = HTTP.get("http://localhost:3000/products/#{selection}")
-    puts "Name: #{response.parse(:json)["name"]}"
-    puts "Price: $#{response.parse(:json)["price"]}"
-    puts "Description: #{response.parse(:json)["description"]}"
-    puts ""
-  elsif selection.downcase == "exit"
-    break
-  else
-    puts "Invalid selection. Try again."
-    puts ""
-  end
-end
+#   if selection == "0"
+#     response = HTTP.get("http://localhost:3000/products")
+#     index = 0
+#     while index < response.parse(:json).length
+#       puts "#{index + 1}. #{response.parse(:json)[index]["name"]}"
+#       index += 1
+#     end
+#   elsif selection == "1" || selection == "2" || selection == "3"
+#     response = HTTP.get("http://localhost:3000/products/#{selection}")
+#     puts "Name: #{response.parse(:json)["name"]}"
+#     puts "Price: $#{response.parse(:json)["price"]}"
+#     puts "Description: #{response.parse(:json)["description"]}"
+#     puts ""
+#   elsif selection.downcase == "exit"
+#     break
+#   else
+#     puts "Invalid selection. Try again."
+#     puts ""
+#   end
+# end
+
+# Testing patch request
+HTTP.patch("http://localhost:3000/products/6.json")
+
+# Testing delete request
+HTTP.delete("http://localhost:3000/products/6.json")
