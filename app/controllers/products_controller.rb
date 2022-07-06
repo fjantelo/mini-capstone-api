95d4f27b3625453098ed7fc4bf23c918e73fe3ba
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-06 14:06:36 -0400

    Adds jb gem, adds show and index templates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    # render json: products.as_json
+    render template: "products/index"
   end
 
   def show
-    product = Product.find_by(id: params["id"])
-    render json: product.as_json(methods: [:friendly_created_at, :is_dicounted?, :tax, :total])
+    @product = Product.find_by(id: params["id"])
+    # render json: product.as_json(methods: [:friendly_created_at, :is_dicounted?, :tax, :total])
+    render template: "products/show"
   end
 
   def create
