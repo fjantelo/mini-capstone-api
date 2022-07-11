e78a40ac2b2ebc3942fa3433c2e8834a0c7efd5e
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-11 13:37:02 -0400

    Adds Image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_185515) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_172947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-product = Product.create(name: "Sony WH-CH510", price: 60, image_url: "https://m.media-amazon.com/images/I/81di9mF9OVL._AC_SX355_.jpg", description: "Sony bluetooth on-ear headphones")
-product = Product.create(name: "Lodge L8SK3", price: 20, image_url: "https://m.media-amazon.com/images/I/71NwXbY+6nL._AC_SX679_.jpg", description: "Lodge L8SK3 10-1/4-Inch Pre-Seasoned Skillet")
-product = Product.create(name: "Ray-Ban RB2180", price: 88, image_url: "https://m.media-amazon.com/images/I/61f5CFTEx3L._AC_UX625_.jpg", description: "Ray-Ban RB2180 Round Sunglasses, Tortoise/Blue Gradient Mirror, 49 mm")
+# product = Product.create(name: "Sony WH-CH510", price: 60, image_url: "https://m.media-amazon.com/images/I/81di9mF9OVL._AC_SX355_.jpg", description: "Sony bluetooth on-ear headphones")
+# product = Product.create(name: "Lodge L8SK3", price: 20, image_url: "https://m.media-amazon.com/images/I/71NwXbY+6nL._AC_SX679_.jpg", description: "Lodge L8SK3 10-1/4-Inch Pre-Seasoned Skillet")
+# product = Product.create(name: "Ray-Ban RB2180", price: 88, image_url: "https://m.media-amazon.com/images/I/61f5CFTEx3L._AC_UX625_.jpg", description: "Ray-Ban RB2180 Round Sunglasses, Tortoise/Blue Gradient Mirror, 49 mm")
+
+Image.create(url: "https://m.media-amazon.com/images/I/81di9mF9OVL._AC_SX355_.jpg") #1
+Image.create(url: "https://m.media-amazon.com/images/I/71NwXbY+6nL._AC_SX679_.jpg") #2
+Image.create(url: "https://m.media-amazon.com/images/I/61f5CFTEx3L._AC_UX625_.jpg") #3
+Image.create(url: "https://m.media-amazon.com/images/I/817EuMPlOOL._AC_SL1500_.jpg") #5
+Image.create(url: "https://wset.com/resources/media/5dfd4ef9-ab45-472e-b28e-234e8204a6bf-large3x4_trashpileup.jpg?1613790746087") #7
