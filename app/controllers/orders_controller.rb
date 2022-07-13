7c9559d068822e3aa187a1994a5eb67c8449652a
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-13 13:24:25 -0400

    Updates create method for orders to get user_id, subtotal, tax, and price from current user and product class

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,13 +10,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params["product_id"])
     order = Order.new(
-      user_id: params["user_id"],
+      user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      subtotal: params["subtotal"],
-      tax: params["tax"],
-      total: params["total"],
+      subtotal: product.price,
+      tax: product.tax,
+      total: product.total,
     )
     order.save
     render json: { "message": "Order created" }
