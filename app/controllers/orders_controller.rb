9934ed3625be52cbe70366e1c35e90299bc7c333
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-13 13:33:25 -0400

    Updates orders show and index methods so only current user can see their orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,17 @@
 class OrdersController < ApplicationController
   def index
-    order = Order.all
+    order = Order.where(user_id: current_user.id)
     render json: order.as_json
   end
 
   def show
+    authenticate_user
     order = Order.find_by(id: params["id"])
-    render json: order.as_json
+    if order.user_id == current_user.id
+      render json: order.as_json
+    else
+      render json: { "message": "Unauthorized user" }
+    end
   end
 
   def create
