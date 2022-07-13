1748386fba483710bd403bcac2fb9c070d88a451
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-13 14:55:28 -0400

    Updates schema

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,41 @@
 class OrdersController < ApplicationController
   def index
-    orders = Order.where(user_id: current_user.id)
-    render json: orders.as_json
+    if current_user
+      orders = Order.where(user_id: current_user.id) # can also do current_user.orders
+      render json: orders.as_json
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def show
-    authenticate_user
-    order = Order.find_by(id: params["id"])
-    if order.user_id == current_user.id
+    if current_user
+      order = current_user.orders.find_by(id: params["id"])
       render json: order.as_json
     else
-      render json: { "message": "Unauthorized user" }
+      render json: [], status: :unauthorized
     end
   end
 
   def create
-    product = Product.find_by(id: params["product_id"])
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
+    if current_user
+      product = Product.find_by(id: params["product_id"])
+      calculated_subtotal = product.price * params[:quantity].to_i
+      calculated_tax = calculated_subtotal * 0.09
+      calculated_total = calculated_subtotal + calculated_tax
 
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
-    )
-    order.save
-    render json: order.as_json
+      order = Order.new(
+        user_id: current_user.id,
+        product_id: params["product_id"],
+        quantity: params["quantity"],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total,
+      )
+      order.save
+      render json: order.as_json
+    else
+      render json: [], status: :unauthorized
+    end
   end
 end
