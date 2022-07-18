f74deae8dcd3729798b1795e400366ead841d9f4
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-18 15:58:49 -0400

    Adds destroy action to carted products controller and route

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -15,4 +15,11 @@ class CartedProductsController < ApplicationController
     carted_products = current_user.carted_products.where(status: "carted")
     render json: carted_products.as_json
   end
+
+  def destroy
+    carted_product = CartedProduct.find_by(id: params[:id])
+    carted_product.status = "removed"
+    carted_product.save
+    render json: { status: "Carted product successfully removed" }
+  end
 end
